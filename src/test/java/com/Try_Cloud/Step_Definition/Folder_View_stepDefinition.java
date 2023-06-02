05621c7f7de35bc373b48f08c2e8f73009999c67
Tolga Can Arık <tolga.c.arik@gmail.com>
2023-06-02 16:45:33 +0300

    folder view

---
+++ src/test/java/com/Try_Cloud/Step_Definition/Folder_View_stepDefinition.java
@@ -0,0 +1,20 @@
+package com.Try_Cloud.Step_Definition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Folder_View_stepDefinition {
+    @Given("user is file page")
+    public void userIsFilePage() {
+
+    }
+
+    @When("user clicks on name button on file page to the change folder view order by Name")
+    public void userClicksOnNameButtonOnFilePageToTheChangeFolderViewOrderByName() {
+    }
+
+    @Then("user should be able to see the list in alphabetical order")
+    public void userShouldBeAbleToSeeTheListInAlphabeticalOrder() {
+    }
+}
